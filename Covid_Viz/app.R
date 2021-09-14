8b3d05473fd25315a6464ef7ad7d8db193489543
huangxe19 <filament1224@hotmail.com>
2021-09-14 16:14:48 -0400

    standarize variable names

--- Covid_Viz/app.R
+++ Covid_Viz/app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(shinythemes)
 library(ggplot2)
 library(dplyr)
+library(stringr)
 library(scales)
 library(ggiraphExtra) 
 library(shinydashboard)
@@ -16,20 +17,12 @@ library(tidyverse)
 
 # load data
 
-df <- read_csv("data/final_daily.txt") %>%
-  mutate(Region = ifelse(is.na(Region), "Caribbean island and unincorporated territory", Region)) %>%
-  mutate(cases = ifelse(is.na(cases), 0, cases)) %>%
-  drop_na(population, median.income, unemployed.rate) %>%
-  mutate(case_per = cases/population*1000)
-
-df$income_group <- cut(df$median.income,
-                       breaks = c(0,40100,120400,Inf), 
-                       labels = c('Low Income','Middle Income','Upper Income'))
+df <- read_csv("data/final.csv")
 
 counties <- get_urbn_map(map = "counties", sf = TRUE)
 total <- merge(counties, df, by.x = "county_fips", by.y = "fips")
 total_rep <- total %>% 
-  select(c("case_per","income_group","unemployed.rate"))
+  select(c("Case.per","Income.Group","Unemployed.Rate"))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -94,10 +87,10 @@ ui <- fluidPage(
                           # select peak
                           checkboxGroupInput(inputId = "s_peak", 
                                       label = ("Select Peak(s)"), 
-                                      choices = c("Peak 1 (7/20/20)" ="peak 1",
-                                                  "Peak 2 (1/8/21)" = "peak 2",
-                                                  "Peak 3 (8/27/21)" ="peak 3"), 
-                                      selected = unique(df$peaks)[1]),
+                                      choices = c("Peak 1 (7/20/20)" ="Peak 1",
+                                                  "Peak 2 (1/8/21)" = "Peak 2",
+                                                  "Peak 3 (8/27/21)" ="Peak 3"), 
+                                      selected = unique(df$Peaks)[1]),
                           
                           # select region
                           selectInput(inputId = "s_region", 
@@ -120,9 +113,9 @@ ui <- fluidPage(
                           # select factor
                           radioButtons(inputId = "s_factor", 
                                        label = ("Select a Factor"), 
-                                       choices = c("Median Income" = "median.income", 
-                                                   "% Democrat Votes" = "per_dem"), 
-                                       selected = "median.income"),
+                                       choices = c("Median Income" = "Median.Income", 
+                                                   "% Democrat Votes" = "Per.Vote.Dem"), 
+                                       selected = "Median.Income"),
                           
                           # select range to display
                           sliderInput(inputId = "slider", 
@@ -156,11 +149,11 @@ ui <- fluidPage(
                                       
                                       radioButtons(inputId ="peaks2",
                                                    label = "",
-                                                   choices = c("Peak 1 (7/20/20)" = "peak 1",
-                                                               "Peak 2 (1/8/21)" = "peak 2",
-                                                               "Peak 3 (8/30/21)" = "peak 3"
+                                                   choices = c("Peak 1 (7/20/20)" = "Peak 1",
+                                                               "Peak 2 (1/8/21)" = "Peak 2",
+                                                               "Peak 3 (8/30/21)" = "Peak 3"
                                                    ),
-                                                   selected = "peak 1"))
+                                                   selected = "Peak 1"))
                                
                                
                         ),#End Left Column Time Period
@@ -169,7 +162,7 @@ ui <- fluidPage(
                                div(align = "left",h3("Data"),
                                    varSelectInput("variable", "Variable:",
                                                   total_rep,
-                                                  selected = "case_per"),
+                                                  selected = "Case.per"),
                                   
                                )# end div
                         ),#end middle Region
@@ -192,27 +185,27 @@ server <- function(input, output) {
   selectedData <- reactive({
     df %>%
       filter(Region %in%  input$s_region,
-             peaks %in% input$s_peak,
-             urban_code  %in% input$s_urban
+             Peaks %in% input$s_peak,
+             Urban.Code  %in% input$s_urban
       )
   
   })
   
   # dotplot output
   output$dotplot <- renderPlot({
-    ggplot(selectedData(), aes_string(x = input$s_factor, y = "case_per",
+    ggplot(selectedData(), aes_string(x = input$s_factor, y = "Case.per",
                                       color = "Region", group = "Region")) +
       geom_point(alpha = 0.5, size = 2.5) +
       geom_smooth(se = FALSE, method = "lm", size = 0.7) +
       theme_bw() +
-      labs(x = if(input$s_factor == "median.income"){"Median Income"} else {"% Deomcrat Votes"},
+      labs(x = if(input$s_factor == "Median.Income"){"Median Income"} else {"% Deomcrat Votes"},
            y = "Covid Case per 1000",
            title = paste0("Covid Case per 1000 people vs. ", 
-                     if(input$s_factor == "median.income"){"Median Income"}
+                     if(input$s_factor == "Median.Income"){"Median Income"}
                       else {"% Deomcrat Votes"})) +
       theme(plot.title = element_text(size = 25)) +
       scale_y_continuous(limits = input$slider) +
-      facet_wrap(~peaks) 
+      facet_wrap(~Peaks) 
   })
   
   # text output by brushing
@@ -220,7 +213,7 @@ server <- function(input, output) {
   
   observeEvent(input$dot_brush,{
     selected_points(brushedPoints(selectedData() %>%
-                                    select(county, Region, peaks, state, !!input$s_factor, case_per), 
+                                    select(County, Region, Peaks, State, !!input$s_factor, Case.per), 
                                   input$dot_brush))
   })
   
@@ -230,10 +223,8 @@ server <- function(input, output) {
     if (!is.null(input$dot_brush)) {
     print(selected_points() %>%
             as.data.frame() %>%
-            mutate(case_per = round(case_per, 2)) %>%
-            rename(`cases per 1000 people` = case_per,
-                   region = Region) %>%
-            arrange(desc(`cases per 1000 people`)), 
+            mutate(Case.per = round(Case.per, 2)) %>%
+            arrange(desc(Case.per)), 
           row.names = FALSE)
     }
       else {cat("Click and drag on point(s) to see details")}
@@ -243,7 +234,7 @@ server <- function(input, output) {
   # filter data according to input choices for map
   selectedData_new <- reactive({
     total %>%
-      filter(peaks == input$peaks2)
+      filter(Peaks == input$peaks2)
   })
   
   # map output
@@ -252,7 +243,7 @@ server <- function(input, output) {
       geom_sf(selectedData_new(), mapping = aes(fill = !!input$variable), color = "grey", lwd = 0.01) +
       theme_bw()+
       theme(axis.text.x = element_blank(),axis.text.y = element_blank()) +
-      if(!!input$variable == "case_per"){scale_fill_viridis_c(option = "magma",limits = c(0,3))}else{}
+      if(!!input$variable == "Case.per"){scale_fill_viridis_c(option = "magma",limits = c(0,3))}else{}
   })
   
 }#End server
